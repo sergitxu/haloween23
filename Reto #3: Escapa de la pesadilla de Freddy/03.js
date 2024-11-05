5e2ab85f2d0e0d29299e1df76f64d7e5a428104a
Sergitxu <sergiortiz@gmail.com>
2024-11-05 19:46:10 +0100

    Prepare 03

--- Reto #3: Escapa de la pesadilla de Freddy/03.js
+++ Reto #3: Escapa de la pesadilla de Freddy/03.js
@@ -1,4 +1,19 @@
 function findSafestPath(dream) {
-    // Code here
-    return 0
-  }
+  // Code here
+  return 0
+}
+
+// TESTS
+const assert = require('assert');
+
+try {
+  const dream = [
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1],
+  ]
+    assert.strictEqual(findSafestPath(dream), 7);
+    console.log('Test ok.');
+} catch (error) {
+    console.error('Failed test:', error);
+}
